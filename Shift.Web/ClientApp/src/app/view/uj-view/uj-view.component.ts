ea10eacb97c724e91cfda93f7504995872f2f379
Dzianis.Lukayenka <Dzianis.Lukayenka@thomsonreuters.com>
2020-05-02 00:01:14 +0300

    feat: updated form for undergraduate

--- Shift.Web/ClientApp/src/app/view/uj-view/uj-view.component.ts
+++ Shift.Web/ClientApp/src/app/view/uj-view/uj-view.component.ts
@@ -1,10 +1,13 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, NgZone, ViewChild } from "@angular/core";
 import { StudentState, selectUJournal } from "src/app/core/store/student/student.state";
 import { Store, select } from "@ngrx/store";
 import { UJournal } from "src/app/infrastracture/entities/ujournal/UJournal";
 import { StorageService } from "src/app/services/storage/storage.service";
 import { UserIdKey } from "src/app/services/storage/StorageKeys";
 import { LoadUJournal } from "src/app/core/store/student/student.actions";
+import { FormBuilder, FormGroup } from "@angular/forms";
+import { take } from "rxjs/operators";
+import { CdkTextareaAutosize } from "@angular/cdk/text-field";
 
 @Component({
     selector: 'pac-uj-view',
@@ -13,14 +16,48 @@ import { LoadUJournal } from "src/app/core/store/student/student.actions";
 })
 export class UndergraduateJournalComponent implements OnInit {
     public journal: UJournal;
-
-    constructor(private studentState: Store<StudentState>, private storage: StorageService) {
-        this.studentState.pipe(select(selectUJournal)).subscribe(j => this.journal = j);
+    public journalOptions: FormGroup;
+    
+    constructor(
+        private studentState: Store<StudentState>, 
+        private storage: StorageService,
+        private fb: FormBuilder,
+    ) {
+        this.studentState.pipe(select(selectUJournal)).subscribe(j => {
+            if(!!j && !!this.journalOptions) {
+                this.journal = j;
+                this.journalOptions.patchValue(this.journal);
+            }
+        });
     }
 
     ngOnInit() {
         let userId = +this.storage.getValue(UserIdKey);
 
         this.studentState.dispatch(new LoadUJournal({ userId: userId }));
+        this.initializeForm();
+    }
+
+    public initializeForm() {
+        this.journalOptions = this.fb.group({
+            PreparationInfo: this.fb.group({
+                PreparationInfoId: [''],
+                Topic: [''],
+                Relevance: [''],
+                Objectives: [''],
+                ResearchProcedure: [''],
+                Additions: [''],
+                PreparationSubmittedDate: [''],
+                PreparationApprovedDate: [{ value: '', disabled: true }],
+                IsPreparationSubmitted: [false],
+                IsPreparationApproved: [false],
+                IsResearchSubmitted: [false],
+                IsResearchApproved: [false],
+            }),
+        })
+    }
+
+    public saveGroup(chapter: string){
+        console.log(this.journalOptions.controls[chapter].value);
     }
 }
--- Shift.Web/ClientApp/src/app/view/view.module.ts
+++ Shift.Web/ClientApp/src/app/view/view.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from "@angular/core";
 import { MaterialModule } from "../material/material.module";
 import { GraduateJournalComponent } from "./gj-view/gj-view.component";
 import { UndergraduateJournalComponent } from "./uj-view/uj-view.component";
+import { ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
-    imports: [MaterialModule],
+    imports: [MaterialModule, ReactiveFormsModule],
     declarations: [
         GraduateJournalComponent,
         UndergraduateJournalComponent,
